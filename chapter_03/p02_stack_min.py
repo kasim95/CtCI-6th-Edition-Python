37485b8b0974ecc4d26e825119b86f722bdce4de
Kasim <panjrikasim@gmail.com>
2020-12-12 19:00:37 -0800

    Optimized Ch3-Q2

--- chapter_03/p02_stack_min.py
+++ chapter_03/p02_stack_min.py
@@ -48,7 +48,57 @@ class MultiStack:
         return offset + self.sizes[stacknum] - 1
 
 
-def StackMin():
+# Optimized Solution for this problem
+class StackNode(object):
+    def __init__(self, value, next=None, prev=None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+class StackMin(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.array = [0] * self.capacity
+        self._min = []
+    
+    def isFull(self):
+        return self.size == self.capacity
+    
+    def isEmpty(self):
+        return self.size == 0
+    
+    def push(self, value):
+        if self.isFull():
+            raise Exception('Stack is Full')
+        if self.isEmpty():
+            self._min.append(value)
+            self.array[self.size] = value
+            self.size += 1
+        else:
+            if value <= self._min[-1]:
+                self._min.append(value)
+            self.size += 1
+            self.array[self.size-1] = value
+        return True
+        
+    def pop(self):
+        if self.isEmpty():
+            raise Exception("Stack is Empty")
+        popped = self.array[self.size-1]
+        if self._min[-1] == popped.value:
+            _ = self._min.pop()
+        self.array[idx] = 0
+        self.size -= 1
+        return popped.value
+    
+    def min(self):
+        if self.isEmpty():
+            raise Exception("Stack is Empty")
+        return self._min[-1]
+
+
+def multistacksolution():
     newstack = MultiStack(10)
     newstack.Push(5, 0)
     newstack.Push(6, 0)
@@ -63,6 +113,22 @@ def StackMin():
     newstack.Push(2, 0)
     print(newstack.Min(0))
 
+def stackminsolution():
+    newstack = StackMin(10)
+    newstack.push(5)
+    newstack.push(6)
+    newstack.push(2)
+    newstack.push(7)
+    newstack.push(14)
+    newstack.push(3)
+    print(newstack.min())
+    newstack.push(1)
+    newstack.push(4)
+    newstack.push(44)
+    newstack.push(2)
+    print(newstack.min())
 
 if __name__ == "__main__":
-    StackMin()
+    multistacksolution()
+    print('*'*5)
+    stackminsolution()
